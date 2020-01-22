9c3a8d36e5ee6a23f54ca9f9d0ed6e77630794dd
Milad <arbabi.milad@gmail.com>
2020-01-22 12:31:08 +0100

    pushing day 6 and bonus changes

--- client/players/players-entry.js
+++ client/players/players-entry.js
@@ -1,17 +1,16 @@
 const players = require('./players');
-
 const client = require('./players-client');
 
 const onInitialLoad = function(data) {
   const nav = document.querySelector('#players-list');
 
-  const loadButton = document.createElement("button");
+  const loadButton = document.createElement('button');
   loadButton.setAttribute('onclick', 'loadMore();');
   loadButton.innerText = 'Load more';
   nav.appendChild(loadButton);
   
   players.appendTo(document.querySelector('#players-list ul'), data);
-}
+};
 
 client.fetchPlayers(onInitialLoad);
 
@@ -27,11 +26,11 @@ const loadMore = function() {
 
   client.fetchPlayers(function(data) {
     if (!data.next) {
-      document.querySelector('#players-list >button').style.display = 'none'; 
+      document.querySelector('#players-list > button').style.display = 'none'; 
     }
     players.appendTo(document.querySelector('#players-list ul'), data);
   }, page);
-}
+};
 
 window.addPlayerData = addPlayerData;
 window.loadMore = loadMore;
